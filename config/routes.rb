dd19fe92f251b700a592a46e55c298d4df597818
Dongeun <cscid170319@ajou.ac.kr>
2020-10-29 00:43:00 +0900

    init

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,8 @@ Rails.application.routes.draw do
 	root 'main#index'
 	
 	resources :members
+	resources :restaurants, only:[:index, :create, :show]
 	resources :sessions, only:[:new, :create, :destroy]
 	
 	get 'about/index' => 'about#index'
-	
-	get 'restaurants/index' => 'restaurants#index'
-	post 'restaurants/create' => 'restaurants#create'
 end
