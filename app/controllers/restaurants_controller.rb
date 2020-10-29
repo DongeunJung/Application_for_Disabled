f72a1749283ae027114886689ee9ed12da563e21
Dongeun <cscid170319@ajou.ac.kr>
2020-10-29 12:00:25 +0900

    init

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,23 +1,7 @@
 class RestaurantsController < ApplicationController
 	def index
-		xml = RestClient.get 'http://openapi.seoul.go.kr:8088/51565a7a656373633636764e475464/xml/touristFoodInfo/1/414/'
-		hash = (Hash.from_xml(xml).to_a)[0]
-		hash4 = hash[1].to_a
-		@hash5 = hash4[2][1]
+		@restrs = Restr.all
 	end
 	
-	def create
-		Restr.create(restrid:params[:id], name:params[:name], address:params[:address])
-		
-		restr = Restr.find_by(restrid:params[:id])
-		
-		if !restr.nil?
-			RestrDetail.create(restr_id:restr.id, 
-							   fulladd:params[:fulladd], 
-							   accessible:params[:accessible], 
-							   parking:params[:parking], 
-							   isflat:params[:isflat], 
-							   elevator:params[:elevator])
-		end
-	end
+	
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,18 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+xml = RestClient.get 'http://openapi.seoul.go.kr:8088/51565a7a656373633636764e475464/xml/touristFoodInfo/1/414/'
+
+hash = (Hash.from_xml(xml).to_a)[0]
+hash4 = hash[1].to_a
+@hash5 = hash4[2][1]
+
+@hash5.each_with_index do |h,index|
+	output = h.to_a
+	
+	
+	Restr.create(id:index+1, restrid:output[0][1], name:output[1][1], address:output[2][1])
+	RestrDetail.create(restr_id:index+1, 
+					   fulladd:output[3][1], 
+					   accessible:output[7][1], 
+					   parking:output[8][1], 
+					   isflat:output[9][1], 
+					   elevator:output[10][1])
+end
