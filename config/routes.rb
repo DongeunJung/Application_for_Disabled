4639c5fb4c7f2d8855622eec02d20d79e2af2287
Dongeun <cscid170319@ajou.ac.kr>
2020-11-01 11:57:16 +0900

    init

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,15 @@
 Rails.application.routes.draw do
 	root 'main#index'
 	
-	resources :members
+	resources :members do 
+		resources :histories, only:[:update]
+		resources :bookmarks, only:[:update]
+	end
 	resources :restaurants, only:[:index, :show]
 	resources :accomodations, only:[:index, :show]
 	resources :sessions, only:[:new, :create, :destroy]
 	resources :historys, only:[:index]
+	resources :comments
 	
 	get 'about/index' => 'about#index'
 	get 'map/index' => 'map#index'
