bd4e472aa253819d0075a3b891091b325e19b147
Dongeun <cscid170319@naver.com>
2020-11-10 17:17:58 +0900

    init

--- app/controllers/histories_controller.rb
+++ app/controllers/histories_controller.rb
@@ -5,7 +5,12 @@ class HistoriesController < ApplicationController
 	end
 	
 	def destroy
-		History.find(params[:id]).destroy
+		if params[:id] == 'destroy_all'
+			member = Member.find(session[:user_id])
+			member.histories.destroy_all
+		else
+			History.find(params[:id]).destroy
+		end
 		redirect_to histories_path
 	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -15,6 +15,7 @@ Rails.application.routes.draw do
 	
 	resources :sessions, only:[:new, :create, :destroy]
 	resources :histories, only:[:index, :destroy]
+	
 	resources :bookmarks, only:[:index, :destroy]
 	resources :comments
 	resources :about, only:[:index]
